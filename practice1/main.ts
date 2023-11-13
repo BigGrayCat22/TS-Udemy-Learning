dfa8f397fa0bf7e4d02f53757877b2dcdc70e78f
Denis Pavlov <dpavlov2208@gmail.com>
2023-11-13 17:18:38 +0200

    techCommit

---
+++ practice1/main.ts
@@ -0,0 +1,24 @@
+let info: {
+    officeID: number,
+    isOpened: boolean,
+    contacts: {
+        email: string,
+        phone: string,
+        adress: {
+            city: string,
+            index: number
+        }
+    }
+} = {
+    "officeID": 45,
+    "isOpened": false,
+    "contacts": {
+        "email": "myMail",
+        "phone": "1+380961234567",
+        "adress": {
+            "city": "Cherkassy",
+            "index": 18008
+        }
+    }
+
+}
